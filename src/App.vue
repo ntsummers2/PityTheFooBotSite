fa7f581727dc3a90bc92ac0a1296cf7422a925f6
summer68 <summer68@msu.edu>
2017-12-28 14:26:56 -0500

    Create fooBot website with firebase and vuejs.

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <video autoplay loop id='pityDaFooVideo'>
+    <source src='static/shutupfoo.mp4' type='video/mp4'>
+  </video>
+</template>
+
+<script src='https://www.gstatic.com/firebasejs/4.8.1/firebase.js'></script>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style>
+html, body {
+  margin:0; padding: 0;
+  height: 100%; width: 100%;
+  background: #000;
+}
+
+#pityDaFooVideo {
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+}
+</style>
